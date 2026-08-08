6bc473ea8125bb2acdb976c74599e4a424dae7ef
fortunejxhn101 <fortunekelechi6@gmail.com>
2026-08-08 11:54:30 +0200

    First Commit:Second commit

---
+++ QueueLessQMS1/src/main/java/za/ac/cput/queuelessqms1/domain/Patient.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.cput.queuelessqms1.domain;
+
+/**
+ *
+ * @author admin
+ */
+public class Patient {
+    
+}
